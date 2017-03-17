b2a1c0077fcd8f826120d04ad58533fa1f41c16e
michaelyin <gt8442b@yahoo.com>
2017-03-16 20:38:50 -0400

    add log.

--- src/main/java/net/wyun/wcrs/controller/WechatController.java
+++ src/main/java/net/wyun/wcrs/controller/WechatController.java
@@ -3,6 +3,12 @@
  */
 package net.wyun.wcrs.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 import net.wyun.wcrs.jsj.JSJFormInfo;
 import net.wyun.wcrs.model.User;
 import net.wyun.wcrs.model.UserRepository;
+import net.wyun.wcrs.wechat.SignUtil;
 
 /**
  * @author Xuecheng
@@ -33,6 +40,31 @@ public class WechatController {
 	String saveUser(@RequestBody String data){
 		logger.info("wechat data: " + data);
 		return data;
+	}
+	
+	@RequestMapping(value= "/wechat", method=RequestMethod.GET)
+	void handShake(HttpServletRequest request, HttpServletResponse response) throws IOException{
+		// 微信加密签名
+		String signature = request.getParameter("signature");
+		// 时间戳
+		String timestamp = request.getParameter("timestamp");
+		// 随机数
+		String nonce = request.getParameter("nonce");
+		// 随机字符串
+		String echostr = request.getParameter("echostr");
+		
+		logger.debug("signature, timestamp, echostr: " + signature + ", " + timestamp + ", " + echostr);
+
+		PrintWriter out = response.getWriter();
+		// 请求校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败
+		if (SignUtil.checkSignature(signature, timestamp, nonce)) {
+			out.print(echostr);
+		}else{
+			logger.info("signature check fails!" + request.getRemoteHost());
+		}
+		out.close();
+		out = null;
+
 	}
 
 }
