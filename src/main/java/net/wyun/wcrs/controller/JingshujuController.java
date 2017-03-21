03bb525962ba377f9b3362d283d9c8a3153741e8
michaelyin <gt8442b@yahoo.com>
2017-03-21 16:35:24 -0400

    add handling for jinshuju's data.

--- src/main/java/net/wyun/wcrs/controller/JingshujuController.java
+++ src/main/java/net/wyun/wcrs/controller/JingshujuController.java
@@ -3,6 +3,8 @@
  */
 package net.wyun.wcrs.controller;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,15 +13,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import net.wyun.wcrs.jsj.JSJFormInfo;
 import net.wyun.wcrs.model.User;
 import net.wyun.wcrs.model.UserRepository;
+import net.wyun.wcrs.model.UserStatus;
 
 /**
  * @author Xuecheng
  *
  */
+
 @RequestMapping("/secure")
 @RestController
 public class JingshujuController {
@@ -29,10 +34,20 @@ public class JingshujuController {
 	@Autowired
 	UserRepository userRepo;
 	
+	@CrossOrigin
 	@RequestMapping(value= "/jsj", method=RequestMethod.POST)
 	String saveUser(@RequestBody JSJFormInfo data){
 		logger.info("jingshuju data: " + data.getFormName() + data.getEntry());
-		return data.getForm();
+		
+		String oid = data.getEntry().getXFieldWeixinOpenid();
+		User u = userRepo.findByOpenID(oid);
+		if(null == u){
+			throw new RuntimeException("user not exist for oid: " + oid);
+		}
+		u.setStatus(UserStatus.REGISTERED);
+		u.setModify_t(new Date());
+		userRepo.save(u);
+		return oid;
 	}
 
 }
