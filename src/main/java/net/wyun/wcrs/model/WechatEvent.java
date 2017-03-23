290ab7b329220d24316e2beb1ad703e1893eeced
michaelyin <gt8442b@yahoo.com>
2017-03-23 12:28:33 -0400

    handle wechat event.

---
+++ src/main/java/net/wyun/wcrs/model/WechatEvent.java
@@ -0,0 +1,116 @@
+/**
+ * 
+ */
+package net.wyun.wcrs.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+/**
+ * @author Xuecheng
+ *
+ */
+@Entity
+@Table(name = "wc_event")
+public class WechatEvent {
+	
+	public WechatEvent(String toUserName, String fromUserName, String msgType, String event, String eventKey) {
+		this.toUserName = toUserName;
+		this.fromUserName = fromUserName;
+		this.msgType = msgType;
+		this.event = event;
+		this.eventKey = eventKey;
+		this.create_t = new Date();
+	}
+
+	public WechatEvent(){}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id; 
+
+	public Long getId() {
+		return id;
+	}
+
+	@SuppressWarnings("unused")
+	private void setId(Long id) {
+		this.id = id;
+	}
+	
+	@Column(name = "to_user_name")
+    private String toUserName; 
+	
+	@Column(name = "from_user_name")
+    private String fromUserName; 
+	
+	@Column(name = "msg_type")
+    private String msgType; 
+	
+	@Column(name = "create_t")
+    private Date create_t; 
+	
+    private String event;
+    
+    @Column(name = "event_key")
+    private String eventKey;
+
+	public String getToUserName() {
+		return toUserName;
+	}
+
+	public void setToUserName(String toUserName) {
+		this.toUserName = toUserName;
+	}
+
+	public String getFromUserName() {
+		return fromUserName;
+	}
+
+	public void setFromUserName(String fromUserName) {
+		this.fromUserName = fromUserName;
+	}
+
+	public String getMsgType() {
+		return msgType;
+	}
+
+	public void setMsgType(String msgType) {
+		this.msgType = msgType;
+	}
+
+	public Date getCreate_t() {
+		return create_t;
+	}
+
+	public void setCreate_t(Date create_t) {
+		this.create_t = create_t;
+	}
+
+	public String getEvent() {
+		return event;
+	}
+
+	public void setEvent(String event) {
+		this.event = event;
+	}
+
+	public String getEventKey() {
+		return eventKey;
+	}
+
+	public void setEventKey(String eventKey) {
+		this.eventKey = eventKey;
+	}
+    
+}
---
+++ src/main/java/net/wyun/wcrs/model/WechatEventRepository.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package net.wyun.wcrs.model;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * @author Xuecheng
+ *
+ */
+public interface WechatEventRepository extends CrudRepository<WechatEvent, Long>{
+	
+}
